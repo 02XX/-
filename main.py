fcd35acb422c2fb94c8abcf8f4bbb0619ad080b0
02XX <1101533765@qq.com>
2021-02-02 01:15:08 +0800

    test
    Signed-off-by: 02XX <1101533765@qq.com>

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ class MainWindows(QMainWindow):
         self.ui.question.append("发生错误，错误如下")
         self.ui.answer.append(e)  
 if __name__ == "__main__":
+    # 创建GUI
     app = QApplication(sys.argv)
     window = MainWindows()
     window.show()
